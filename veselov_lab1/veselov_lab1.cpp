7cdab8406af09f3dfc45f784f7f6967760d498e8
Тимур Хайретдинов <khairetdinovtimur123@yandex.ru>
2022-10-29 13:06:17 +0300

    last commit

--- veselov_lab1/veselov_lab1.cpp
+++ veselov_lab1/veselov_lab1.cpp
@@ -1,55 +1,43 @@
 ﻿#include <iostream>
 #include <cmath>
 #include <time.h>
-#define M_PI 3.14159265358979323846
-#define _USE_MATH_DEFINES
 
 using namespace std;
 
 int main()
 {
+    cout << "Input argument X: (x->0)" << endl;
+    cout << "-->";
+    double x;
+    cin >> x;
+    double start_programm = clock();
     double ch_b = 1.0;
     double ch_a = 1.0;
     double zn = 1.0;
     double constanta = (3.0 / 4.0);
-    double x;
-    cin >> x;
     double ex = pow(10, -15);
     double slagaemoe = -x;
-    double i = 3.0;
+    int i = 3;
     int kolvo_slag = 1;
     double rezultat = 0;
-    double y = x;
-    slagaemoe = -y;
+    double abs_pogr = 0;
+    double otn_pogr = 0;
     while (fabs(slagaemoe) >= ex) {
-        //double chislitel = (pow(3,2));
         ch_a *= (3 * 3);
         zn *= (i * (i - 1));
-        slagaemoe *= ((-1) * (y *y) * ((ch_a / zn) + (-ch_b / zn)) * constanta);
-        //cout << slagaemoe << endl;
-        i += 2.0;
+        slagaemoe *= ((-1) * (x *x) * ((ch_a / zn) + (-ch_b / zn)) * constanta);
+        i += 2;
         rezultat += slagaemoe;
+        abs_pogr = fabs(rezultat - pow(sin(x), 3));
+        otn_pogr = fabs(abs_pogr / pow(sin(x), 3)) * 100;
+        double end_programm = clock();
+        cout << "____________________________________________________________" << endl;
         cout << "My_func = " << rezultat << "   Standart_func = " << pow(sin(x), 3) << endl;
+        cout << "Kolvo_slag = " << kolvo_slag << " ABS_POGR = " << abs_pogr << " OTN_POGR = " << otn_pogr << " %" << endl;
+        cout << "Time = " << (double)(end_programm - start_programm) / CLOCKS_PER_SEC << "c" << endl;
+        cout << "____________________________________________________________" << endl;
         kolvo_slag++;
     }
-    cout << kolvo_slag << endl;
-    cout << rezultat << endl;
-    cout << pow(sin(x), 3) << endl;
 
- 
-    
 }
 
-// i c 5 
-//первое слагаемое i = 3
-// домножить на 3*4 поделить на 3*2
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
